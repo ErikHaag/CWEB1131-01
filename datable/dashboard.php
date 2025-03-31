bac72476f70eab763917a3a72fb3bd6341fd60e0
Calculus_is_fun at school! <60990391+ErikHaag@users.noreply.github.com>
2025-03-31 11:04:41 -0500

    Update dashboard.php
    add escape characters to password string

--- datable/dashboard.php
+++ datable/dashboard.php
@@ -79,7 +79,7 @@ if ($role == "") {
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     const userID = <?php echo $_SESSION["user_id"]; ?>;
-    const password = "<?php echo $_SESSION["password"]; ?>";
+    const password = "<?php echo addslashes($_SESSION["password"]); ?>";
 
     function createRow(row) {
         let rowHTML = "<tr>";
